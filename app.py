be954fda3d20048df9d0e5c07d604e3e2595e0c3
0ev <andyye.jongcye@gmail.com>
2021-05-25 16:11:50 +0900

    done
    done

--- app.py
+++ app.py
@@ -12,5 +12,17 @@ def hello():
 def gallery():
     return render_template('gallery.html', mypictures = pictures())
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
